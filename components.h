e1ec8a26209b8df4837584d5bbe3c5e3438d8cc0
gisikbaksa <gisikbaksa@dgist.ac.kr>
2017-05-14 16:32:52 +0900

    just init of simul

--- components.h
+++ components.h
@@ -6,17 +6,16 @@ enum flag { Q = 0, V = 1 }; // has V? or Q?
 enum instruction { IntAlu = 0, MemRead = 1, MemWrite = 2 }; // Instruction Type
 enum is_complete { P = 0, C = 1 }; // P means pending, C means completed
 
-int N; // Window Size
-int reg[16]; // Architectural Register File
+//int N; // Window Size
+//int reg[16]; // Architectural Register File
 
-struct 
+struct RAT
 {
 	bool RF_valid;
 	int Q;
-} rat[16]; // RAT
+}; // RAT
 
-// Copy codes below, and remove this from header !
-struct
+struct RS
 {
 	bool is_valid; // Busy or not
 	enum instruction op;
@@ -39,14 +38,14 @@ struct
 			int q;
 		} data;
 	} oprd_2;
-} ResvStation[N];
+}//ResvStation[N];
 
 int rob_point = 0; // points first element in ROB. Value must come btw 0 and N-1
 
-struct
+struct ROB
 {
 	enum instruction op; // operation
 	int value; // data 
 	int dest; // Destination ( Must point to one of elements in reg[16] !
 	enum is_complete status; // status = P; means pending, status = C; means completed
-} rob[N];
+};
