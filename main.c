094921dda5165f78282b100a822b9dc8b4504307
gisikbaksa <gisikbaksa@dgist.ac.kr>
2017-05-14 15:03:14 +0900

    main design

--- main.c
+++ main.c
@@ -1,19 +1,40 @@
 // Computer Architecture Homework #2
 // Noh Dong Ju & Park Kyung Won & Yu Jin woo
 
+#include <stdio.h>
 #include <stdlib.h>
+//#include "read.h" //read config and instruction
+//#include "simul.h"//func related simulation
 
-// Read config file
-void* config = read_config('our_config_file.txt');
+typedef struct Config { int justtest;  } Config;
 
-
-// simulate and for every config combination
-for (int index_conf = -1; index_conf < num_of_conf; ++index_conf)
+void main(int argc, char* argv[])
 {
+	//
+	// Read config file and make config array
+	//
+	struct Config* configs;
+	if (argc == 1)
+	{//if there is no input
+		configs = malloc(sizeof(struct Config));
+		read_config("our_config_file.txt",configs);
+	}
+	else
+	{//if there is input from argv
+		configs = malloc(sizeof(struct Config)*(argc-1));
+		for (int idx = 1; idx < argc; )//every config_file 
+			read_config(argv[idx], (configs+idx) );//config array elem init
+	}
+
+	//
+	// simulate and for every config combination
+	//
+	for (int idx_conf = 0; idx_conf < argv ; ++idx_conf)
+	{
+		simul_ooo(configs[idx_conf]);//simulate
+	}
+
 	
-	//simulate using [index_conf]th config and save report_data
-	simul_ooo(config[index_conf]);
+	free(configs);// free config
 
 }
-// free config
-free(config)
